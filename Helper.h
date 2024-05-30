db719a8573719a2ee8b800cbb17331b05725500d
Sophana-Chhiv <83583671+Sophana-Chhiv@users.noreply.github.com>
2024-05-30 17:32:33 +0930

    Add files via upload

--- Helper.h
+++ Helper.h
@@ -2,9 +2,9 @@
 #define HELPER_H
 #include <tuple>
 #include "GridUnit.h"
-#include "ctime"
 #include<random>
 #include <cmath>
+#include <cstdlib>
 
 using namespace std;
 
@@ -13,20 +13,20 @@ class Helper
 public:
     static tuple<int, int> generateRandomCoordinates(int gridWidth, int gridHeight)
     {
-        tuple <int, int> randPos;
-        get<0>(randPos) = rand() % gridWidth + 1;
-        get<1>(randPos) = rand() % gridHeight + 1;
-        return randPos;
+        int x = std::rand() % gridWidth;
+        int y = std::rand() % gridHeight;
+        return std::make_tuple(x, y);
 
     }
     static double calculateDistance(tuple<int, int> coords1, std::tuple<int, int> coords2)
     {
-        int dx = get<0>(coords1) - get<0>(coords2);
-        int dy = get<1>(coords1) - get<1>(coords2);
-        double distance = sqrt(dx * dx + dy * dy);
-        return distance;
+        int x1, y1, x2, y2;
+        std::tie(x1, y1) = coords1;
+        std::tie(x2, y2) = coords2;
+        return sqrt(pow(x2 - x1, 2) + pow(y2 - y1, 2));
     }
 };
+
 
 #endif
 
